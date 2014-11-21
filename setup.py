e9b472f41fdfd80227e93252bb73b34f855493f8
mk <manojklm@gmail.com>
2014-11-22 01:09:02 +0530

    Remove python 3.3 support
    Removing 3.3 due to failing tests

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ CLASSIFIERS = [
   'Operating System :: OS Independent',
   'Programming Language :: Python :: 2.6',
   'Programming Language :: Python :: 2.7',
-  'Programming Language :: Python :: 3.3',
   'Programming Language :: Python :: 3.4',
   'Topic :: Software Development :: Testing',
   'Topic :: Software Development :: Libraries :: Python Modules'
