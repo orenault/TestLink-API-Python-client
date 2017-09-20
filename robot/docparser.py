0b897aa8527b5169e6eaf68de25ba596bb1065f9
Brian Williams <brian.williams@actifio.com>
2017-09-20 19:37:54 -0400

    Fix example in docparser docstring

--- robot/docparser.py
+++ robot/docparser.py
@@ -5,7 +5,7 @@ class DocTestParser(object):
     """Find all externaltestcaseid's in a test's docstring.
 
     If your externaltestcaseid prefix is abc and the test has 'abc-123' in it's docstring.
-    `DocTestParser('abc').get_testcases()` would return `['abc-123']`.
+    `DocTestParser('abc').get_testcases(test)` would return `['abc-123']`.
     """
     def __init__(self, doc_matcher=None, doc_matchers=None):
         """
